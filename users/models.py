ea7ea828dbdfc93ac310067ce0ce4adf6540267e
edcilo <edcilop@gmail.com>
2021-03-20 11:53:46 -0600

    move sender email to signals file

--- users/models.py
+++ users/models.py
@@ -1,11 +1,6 @@
 import uuid
 from django.db import models
-from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth.models import AbstractUser
-from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.urls import reverse
-from django_rest_resetpassword.signals import reset_password_token_created
 
 
 # Create your models here.
@@ -21,25 +16,3 @@ class User(AbstractUser):
     updated_at = models.DateTimeField(null=True)
     deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(null=True)
-
-
-@receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-    context = {
-        'current_user': reset_password_token.user,
-        'username': reset_password_token.user.username,
-        'email': reset_password_token.user.email,
-        'reset_password_url': "{}?token={}".format(reverse('users:password_reset:reset-password-request'), reset_password_token.key)
-    }
-
-    email_html_message = render_to_string('emails/user_reset_password.html', context)
-    email_plaintext_message = render_to_string('emails/user_reset_password.txt', context)
-
-    msg = EmailMultiAlternatives(
-        "Password Reset for {title}".format(title="edcilo.com"),
-        email_plaintext_message,
-        "noreply@edcilo.com",
-        [reset_password_token.user.email]
-    )
-    msg.attach_alternative(email_html_message, "text/html")
-    msg.send()
--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,9 @@
+from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django_rest_resetpassword.signals import reset_password_token_created
 
 from .models import User
 
@@ -8,3 +12,25 @@ from .models import User
 def set_new_user_inactive(sender, instance, **kwargs):
     if instance._state.adding is True:
         instance.is_active = False
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    context = {
+        'current_user': reset_password_token.user,
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'reset_password_url': "{}?token={}".format(reverse('users:password_reset:reset-password-request'), reset_password_token.key)
+    }
+
+    email_html_message = render_to_string('emails/user_reset_password.html', context)
+    email_plaintext_message = render_to_string('emails/user_reset_password.txt', context)
+
+    msg = EmailMultiAlternatives(
+        "Password Reset for {title}".format(title="edcilo.com"),
+        email_plaintext_message,
+        "noreply@edcilo.com",
+        [reset_password_token.user.email]
+    )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
