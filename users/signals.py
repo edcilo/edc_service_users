b51dc7073f4decbf08a3c8be192a3a3fbc7edbdd
edcilo <edcilop@gmail.com>
2021-03-20 11:45:59 -0600

    create user deactivated by default

---
+++ users/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from .models import User
+
+
+@receiver(pre_save, sender=User)
+def set_new_user_inactive(sender, instance, **kwargs):
+    if instance._state.adding is True:
+        instance.is_active = False
