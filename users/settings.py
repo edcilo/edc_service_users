6aa7c42613a0f67d5e019d6269a1290ace9894d6
edcilo <edcilop@gmail.com>
2021-03-23 19:43:28 -0600

    add users config file

---
+++ users/settings.py
@@ -0,0 +1,14 @@
+from app.environ import env
+
+
+# USERS_ACTIVATION_ON: If True activation flow is enabled
+USERS_ACTIVATION_ON = False
+
+# USERS_SEND_ACTIVATION_EMAIL: If True when is registered a new user, send automatically an email activation
+USERS_SEND_ACTIVATION_EMAIL = False
+
+# USERS_ACTIVATION_TOKEN_LIFETIME: life time for the token in seconds
+USERS_ACTIVATION_TOKEN_LIFETIME = 600
+
+# USER_ACTIVATION_REQUIRED: If True the user account must be activated for login
+USERS_ACTIVATION_REQUIRED = False
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import users.settings as settings
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -30,6 +31,9 @@ class UserViewSet(viewsets.GenericViewSet):
         serializer = UserSignUpSerializer(data=request.data, context={"request": self.request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
+        # send email notification
+
         return Response(None, status=status.HTTP_201_CREATED)
 
 
@@ -38,4 +42,5 @@ class HelloView(APIView):
 
     def get(self, request):
         content = {'message': 'Hello, World!'}
+        print('--------------------->', settings.USERS_ACTIVATION_ON)
         return Response(content)
